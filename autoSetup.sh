faefcf94c7da246d6954ab3b8e8009cf430ff3c0
Paul Joseph Miloro <pmiloro3@login-s1.pace.gatech.edu>
2019-10-17 23:09:42 -0400

    Debugging complete

--- autoSetup.sh
+++ autoSetup.sh
@@ -4,22 +4,23 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
-
+USERNAME="pmiloro3"
 CACTUS_DIR="$HOME/data/Cactus"
 SETTINGS_DIR="$CACTUS_DIR/simfactory/etc/"
 SIM_OUTPUT_DIR=$(grep -o "\w*$HOME/\w*/\w*" "${SETTINGS_DIR}defs.local.ini")
-STATUS_FILE="status.txt"
+STATUS_FILE="$DIR/status.txt"
 
 
-echo $DIR
-echo $CACTUS_DIR
-echo $SETTINGS_DIR
-echo $SIM_OUTPUT_DIR
+#echo $DIR
+#echo $CACTUS_DIR
+#echo $SETTINGS_DIR
+#echo $SIM_OUTPUT_DIR
 
 previousSims=($(ls $SIM_OUTPUT_DIR))
-for i in "${previousSims[@]}"; do
-	echo "${previousSims[$i]}"
-done
+#for i in "${previousSims[@]}"; do
+#	printVal="$i"
+#	echo "$printVal"
+#done
 
 numPreviousSims=${#previousSims[@]}
 
@@ -34,8 +35,8 @@ previousMaxMs=()
 #Find or create the data for all the simulations tracked in the data source folder
 for i in "${previousSims[@]}"
 do
-	tokenlessPar=$(ls "$SIM_OUTPUT_DIR/$i/" | grep ".par")
-	echo $tokenlessPar
+	tokenlessPar=$(ls "$SIM_OUTPUT_DIR/$i/output-0000/" | grep ".par")
+#	echo "tokenlessPar:$tokenlessPar"
 	parfileNames+=("PFN:$tokenlessPar")
 
 	parSources+=("PSL:Local")
@@ -45,23 +46,30 @@ do
 	dataSourceDirs+=("DSD:$SIM_OUTPUT_DIR/")
 
 
-
-	sims=($(showq | grep $USERNAME))
+#	echo "Checking queue:"
+	sims=("$(showq | grep $USERNAME)")
+#	for sim in "${sims[@]}";do
+#		echo "Contents of sim array are: $sim"
+#	done
 
 	count=0
 	#Check the actual submission queue for more information on the simulations
 	for sim in "${sims[@]}"
 	do 
+#		echo "Simulation is: $sim"
 
-		#Split showq output by the delimiter they use (eight spaces)
-		IFS="\s\s\s\s\s\s\s\s";  read -ra line <<< $sim
+		#Split showq output into an array by spaces
+		line=($sim)
+#		echo "Line is: $line"
+
 		#Grab the jobID and status from this line
 		jobID=${line[0]}
 		jobName=$(checkjob $jobID | grep "AName")
-		jobName=${jobName:0:${#jobName}}
+#		echo "$jobName"
+		jobName=${jobName:7:${#jobName}-1}
+#		echo "$jobName"
 
-
-		if {$i == $jobName}; then
+		if [ "$i" == "$jobName" ]; then
 			count+=1
 
 			jobIDs+=("JID:$jobID")
@@ -73,37 +81,70 @@ do
 			lastRunDates+=("LRD:$lastRunDate")
 		else
 			#Only do this step once to prevent repeated entries
-			if [$count==0]; then
+			if [ $count==0 ]; then
 				count+=1
 				jobIDs+=("JID:")
 				queueStatuses+=("QUS:Unsubmitted")
 				#Get the last date recorded in the last line of the log file for this simulation
-				lastRunDates+=("LRD:$(tac "$SIM_OUTPUT_DIR$i/log.txt" | grep -o -m1 -P "(?<=\[LOG:).*(?=\])")")
+				lastRunDates+=("LRD:$(tac "$SIM_OUTPUT_DIR/$i/log.txt" | grep -o -m1 -P "(?<=\[LOG:).*(?=\])")")
 			fi
 
 		fi
 
-		simLoc="$SIM_OUTPUT_DIR$simName"
-		activeFolder=$(ls "$simLoc/" | grep "active")
-		#Cut the ".par" off of the parfile name to find the directory name of the actual data
-		internalFolderName=${parName:0:${#parName[@]}-5}
-		#Location where the output data for each simulation is stored
-		dataLoc="$simLoc/$activeFolder/$internalFolderName/"
+		simLoc="$SIM_OUTPUT_DIR/$i"
+#		echo "simLoc is: $simLoc"
+		activeFolder="$(ls "$simLoc/" | grep 'active')"
+#		echo "activeFolder is: $activeFolder"
+#		echo "activeFolder length is "${#activeFolder}""
+		folderNumber="${activeFolder:${#activeFolder}-11:${#activeFolder}-14}"
+#		echo "folderNumber is: $folderNumber"
+		readNumber=$(($folderNumber-1))
 
-		#Get the last line of the shift tracker data so we can determine how far the simulation has progressed
-		lastLine=$(tac "${dataloc}ShiftTracker0.asc" | egrep -m 1 .)
-		IFS="\s\t";  read -ra line <<< $lastLine
-		lastM=${line[1]}
-		previousMaxMs+=("PMM:$lastM")
-	done
+		newestFolder="output-$folderNumber"
+#		echo "Newest folder is: $newestFolder"
+		
+                internalFolderName=${tokenlessPar:0:${#tokenlessPar}-4}
+                dataLoc="$simLoc/$newestFolder/$internalFolderName"
 
-	#Create status file
-	touch "$DIR$STATUS_FILE"
-	#Add determined data settings to status file
-	for ((i=0;i<numPreviousSims;i++)); do
-		sed -i "$i a ${parfileNames[$i]}\t${parSources[$i]}\t${simNames[$i]}\t${jobIDs[$i]}\t${dataSourceDirs[$i]}\t${queueStatuses[$i]}\t${lastRunDates[$i]}\t${previousMaxMs[$i]}"
+		#If the most recent output folder is filled out with the latest data
+		if [ -f "$dataLoc/ShiftTracker0.asc" ]; then
+			#Grab it
+                        lastLine=$(tac "$dataLoc/ShiftTracker0.asc" | egrep -m 1 .)
+                        line=($lastLine)
+                        lastM=${line[1]}
+                        previousMaxMs+=("PMM:$lastM")
+		else
+			#If there's only one output folder and no data in it, this simulation hasn't been run before,
+			#so its max M is zero
+			if [ $readNumber == -1 ]; then
+				previousMaxMs+=("PMM:0")
+			else 
+				secondFolder="$(printf '%04d' $readNumber)"
+				secondFolder="output-$secondFolder"
+				dataLoc="$simLoc/$secondFolder/$internalFolderName"
+
+				lastLine=$(tac "$dataLoc/ShiftTracker0.asc" | egrep -m 1 .)
+                        	line=($lastLine)
+                        	lastM=${line[1]}
+                	        previousMaxMs+=("PMM:$lastM")
+			fi
+		fi
 	done
 done
 
+#Create status file, clear it if one already exists
+> "$STATUS_FILE"
+#Add determined data settings to status file
+for ((i=0;i<numPreviousSims;i++)); do
+#	echo "${parfileNames[$i]}"
+#	echo "${parSources[$i]}"
+#	echo "${simNames[$i]}"
+#	echo "${jobIDs[$i]}"
+#	echo "${dataSourceDirs[$i]}"
+#	echo "${queueStatuses[$i]}"
+#	echo "${lastRunDates[$i]}"
+#	echo "${previousMaxMs[$i]}"
 
+	echo "${parfileNames[$i]}	${parSources[$i]}	${simNames[$i]}	${jobIDs[$i]}	${dataSourceDirs[$i]}	${queueStatuses[$i]}	${lastRunDates[$i]}	${previousMaxMs[$i]}" >> $STATUS_FILE
+done
 
